7453c24534fef06f08fa21d31e8d4f6150d3718b
Nestor <nestormpdiaz@gmail.com>
2021-07-28 22:15:12 -0600

    Primer commit

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent  {
+
+  termino: string= '';
+  hayError: boolean= false;
+  capital: Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  buscar( termino:string ){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital( this.termino )
+      .subscribe( capital=> {
+        console.log(capital);
+        this.capital = capital;
+
+    }, ( err ) => {
+      this.hayError = true;
+      this.capital = [];
+    } )
+  }
+  
+  sugerencias( termino: string ){
+    this.hayError = false;
+    //TODO: crear sugerencias
+  }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  exports: [
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
